50ac11ca6985ca22ee24a7bea926bb950ee2b668
Aleksey Fishouk <fishouk@yandex.ru>
2014-08-20 15:46:33 +0400

    v1.0

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-﻿var name = 'Василий';
-var admin = name;
- 
+function add(){
+	var a = document.getElementById('game');
+	var b = document.createElement('li');
+    var d = prompt("Введите слово!");
+	var c = document.createTextNode(d);
+	b.appendChild(c);
+	a.appendChild(b);}
