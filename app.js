09df1b538f91084815ad6e97d4f1fd92c9dfafed
Aleksey Fishouk <fishouk@yandex.ru>
2014-08-20 17:22:05 +0400

    v1.01

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+function doClear(theText){
+    if (theText.value == theText.defaultValue) { theText.value = "" } }
+
+function doDefault(theText){ 
+    if (theText.value == "") { theText.value = theText.defaultValue } }
+
 function add(){
 	var a = document.getElementById('game');
 	var b = document.createElement('li');
-    var d = prompt("Введите слово!");
+    var d = document.getElementById('g_input').value;
 	var c = document.createTextNode(d);
 	b.appendChild(c);
-	a.appendChild(b);}
+	a.appendChild(b);
+}
